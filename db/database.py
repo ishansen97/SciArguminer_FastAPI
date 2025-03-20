2f94f1e55ce284a10084b09e76b4255e3f8d9780
ishansen97 <ishanksen@gmail.com>
2025-03-20 23:28:41 +0530

    Connecting to remote SQL server database

--- db/database.py
+++ db/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mssql+aioodbc://@ISHAN-S-WL/sciarguminer?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+# DATABASE_URL = "mssql+aioodbc://@ISHAN-S-WL/sciarguminer?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+# encoding the password due to the presence of '@' character
+DATABASE_URL = "mssql+aioodbc://sciarguminer_db:sciarguminer%40123@44.196.130.217/sciarguminer?driver=ODBC+Driver+17+for+SQL+Server"
 
 # Create Async Engine
 async_engine = create_async_engine(DATABASE_URL, echo=True)
