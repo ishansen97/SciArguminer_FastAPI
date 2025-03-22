761c485d2c36bb609037c7f8fabc8696320ea373
ishansen97 <ishanksen@gmail.com>
2025-03-22 22:38:22 +0530

    setting base log level as DEBUG

--- logic/fileOperations.py
+++ logic/fileOperations.py
@@ -10,7 +10,6 @@ from config import ConfigManager
 from models.Section import Section
 
 config = ConfigManager().config
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 def save_upload_file(upload_file: UploadFile):
